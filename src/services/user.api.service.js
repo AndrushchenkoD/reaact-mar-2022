0ba8c8c18ab6040f7f05428a5737bb4da9b71c88
AndrushchenkoD <kennybro2016@gmaill.com>
2022-07-22 15:51:16 +0300

    hw3

--- src/services/user.api.service.js
+++ src/services/user.api.service.js
@@ -3,5 +3,5 @@ import axios from "axios";
 let axiosInstance = axios.create({baseURL:'https://jsonplaceholder.typicode.com'});
 
 const getUsers=() => axiosInstance.get('/users');
-const getPosts=(id) =>axiosInstance.get(`/${id}/posts`);
-export {getUsers,getPosts}
+const getPosts=(id)=>axiosInstance.get(`/users/${id}/posts`);
+export {getUsers,getPosts}'f'
