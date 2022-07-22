4eeabc541e6b6c9e3f9151f03c9818ef3c808797
AndrushchenkoD <kennybro2016@gmaill.com>
2022-07-22 19:40:19 +0300

    hw4

---
+++ src/COMPONENT/Services/index.js
@@ -0,0 +1,2 @@
+export * from "./Comments.api.service/Comments.api.service"
+export * from "./Users.api.service/User.api.service"
---
+++ src/COMPONENT/index.js
@@ -0,0 +1,3 @@
+export * from './Users/Users'
+export * from './Comments/Comments'
+export * from './Services/index'
