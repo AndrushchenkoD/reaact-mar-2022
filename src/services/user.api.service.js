f9690926e57b143b7ded68d59616cf90ff02a589
AndrushchenkoD <kennybro2016@gmaill.com>
2022-10-15 18:34:32 +0300

    hw 6 commit 1

--- src/services/user.api.service.js
+++ src/services/user.api.service.js
@@ -4,4 +4,4 @@ let axiosInstance = axios.create({baseURL:'https://jsonplaceholder.typicode.com'
 
 const getUsers=() => axiosInstance.get('/users');
 const getPosts=(id)=>axiosInstance.get(`/users/${id}/posts`);
-export {getUsers,getPosts}'f'
+export {getUsers,getPosts};
